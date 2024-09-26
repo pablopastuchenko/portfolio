873d2ae3db21ebee2e0439ded6196890ba82c787
pastuchenko <pablopastuchenkooo@gmail.com>
2024-09-26 17:53:13 -0300

    first commit

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -123,8 +123,8 @@ const Skills = () => {
   return (
     <Container id="skills">
       <Wrapper>
-        <Title>Skills</Title>
-        <Desc>Here are some of my skills on which I have been working on for the past 2 years.
+        <Title>Habilidades</Title>
+        <Desc>Aqui estão algumas das minhas habilidades nas quais venho trabalhando no último ano.
         </Desc>
         <SkillsContainer>
           {skills.map((skill) => (
