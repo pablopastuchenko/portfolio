c3d33856a163882e7e8331c6705213871f14a002
pastuchenko <pablopastuchenkooo@gmail.com>
2024-10-17 10:12:02 -0300

    sass e calendar

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Experience from "./components/Experience";
 import Education from "./components/Education";
 import ProjectDetails from "./components/ProjectDetails";
 import styled from "styled-components";
+import ExperienceCard from "./components/Cards/ExperienceCard.jsx";
 
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
@@ -38,7 +39,7 @@ function App() {
           <HeroSection />
           <Wrapper>
             <Skills />
-            
+            <ExperienceCard />
           </Wrapper>
           <Projects openModal={openModal} setOpenModal={setOpenModal} />
           <Wrapper>
