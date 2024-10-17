c3abf7774f1899359a0e531f8701033b3b9bf828
pastuchenko <pablopastuchenkooo@gmail.com>
2024-10-17 10:21:50 -0300

    sass e calendar

--- src/data/constants.js
+++ src/data/constants.js
@@ -137,25 +137,18 @@ export const skills = [
 export const experiences = [
   {
     id: 0,
-    img: "https://firebasestorage.googleapis.com/v0/b/flexi-coding.appspot.com/o/flipr.jpeg?alt=media&token=1d72532a-45eb-4c1a-a81a-c9bed9fec543",
-    role: "DevOps & Fullstack Engineering Intern",
-    company: "Flipr Inovations Pvt. Ltd.",
-    date: "Aug 2023 - July 2023",
-    desc: "Working on Flipr Platforms, managing DevOps, and streamlining the process with automation.",
+    img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Flogodownload.org%2Fsenai-logo%2F&psig=AOvVaw2LKNxNxurlyXKQQlKLlPHD&ust=1729257565625000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCJij-crAlYkDFQAAAAAdAAAAABAE",
+    role: "Informática 2019",
+    company: "Senai",
+    date: "Fev 2019 - Dez 2019",
+    desc: "Faziamos aplicações Web",
     skills: [
-      "Docker",
-      "Terraform",
-      "AWS",
-      "EC2",
-      "Portainer",
-      "Nginx",
       "JavaScript",
-      "TypeScript",
-      "Node Js",
-      " Next Js",
+      'CSS',
+      "Node",
     ],
-    doc: "https://media.licdn.com/dms/image/D4D2DAQFlp60ZqHuaFQ/profile-treasury-image-shrink_1280_1280/0/1691180828512?e=1692381600&v=beta&t=mM5Y_NE5EPlQhez5FAN6NLVSKcO_Ojt_9Gq3mnFGkAQ",
   },
+  
   
    
 ];
